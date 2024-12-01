06563fdbdbd34b694202998abbf788b595f32cc3
xiaosong111 <qiegao_ssj@foxmail.com>
2024-12-01 17:35:55 +0800

    cros、项目优雅停止

--- main.go
+++ main.go
@@ -3,17 +3,44 @@ package main
 import (
 	"cicd_test/config"
 	"cicd_test/router"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
 	//初始化配置
 	config.InitConfig()
 	//初始化路由
-	engine := router.SetRouter()
+	r := router.SetRouter()
 	port := config.AppConfig.App.Port
 	if port == "" {
 		port = ":8080"
 	}
 
-	engine.Run(port)
+	srv := &http.Server{
+		Addr:    port,
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
--- router/router.go
+++ router/router.go
@@ -3,11 +3,22 @@ package router
 import (
 	"cicd_test/controllers"
 	"cicd_test/middlewares"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func SetRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	auth := r.Group("/api/auth")
 	{
@@ -24,8 +35,8 @@ func SetRouter() *gin.Engine {
 		api.POST("/articles", controllers.CreateArticle)
 		api.GET("/articles", controllers.GetArticles)
 		api.GET("/articles/:id", controllers.GetArticleById)
-		api.POST("/articles/like", controllers.LikeArticle)
-		api.POST("/articles/unlike", controllers.UnlikeArticle)
+		api.POST("/articles/:id/like", controllers.LikeArticle)
+		api.POST("/articles/:id/unlike", controllers.UnlikeArticle)
 	}
 	return r
 }
